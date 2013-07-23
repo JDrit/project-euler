131e8192c2d74b4a67aefc1c24475792563d1552
JD <jd@csh.rit.edu>
2013-07-23 12:21:30 -0400

    Finished problem 28 in java

---
+++ 028/problem028.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+
+public class problem028 {
+
+  public static void main(String[] args) {
+	  HashSet<Double> numbers = new HashSet<Double>();
+		for (int a = 2 ; a <= 100 ; a++) {
+			for (int b = 2; b <= 100 ; b++) {
+				numbers.add( Math.pow(a, b));
+			}
+		}
+		System.out.println(numbers.size());	
+	}
+}
