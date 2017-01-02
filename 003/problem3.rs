fb613fb8fd75f1a2c0e9ab97cdb887c0e3da3888
Joseph Batchik <jbatchik@apple.com>
2017-01-02 09:48:09 -0800

    debuging #3

--- 003/problem3.rs
+++ 003/problem3.rs
@@ -1,5 +1,36 @@
 
 
 fn main() {
+    println!("{}", usize::max_value());
+    println!("{}", LIMIT_SIZE);
     const LIMIT: i64 = 600851475143;
+    const LIMIT_SIZE: usize = 600851475143;
+    let mut results: Vec<i64> = Vec::new();
+    let mut sieve: [bool; LIMIT_SIZE] = [false; LIMIT_SIZE];
+
+    let mut x: i64 = 1;
+    let mut y: i64 = 1;
+
+    while x * x < LIMIT {
+        while y * y < LIMIT {
+            println!("{} {}", x, y);
+            let n = (4 * x * x) + (y * y);
+            if n < LIMIT && (n % 12 == 1 || n % 12 == 5) {
+                //sieve[n as usize] ^= true;            
+            }
+            let m = (3 * x * x) + (y * y);
+            if n < LIMIT && n % 12 == 7 {
+                //sieve[n as usize] ^= true;
+            }
+            let o = (3 * x * x) - (y * y);
+            if x > y && o < LIMIT && o % 12 == 11 {
+                //sieve[n as usize] ^= true;                
+            }
+
+            y += 1;
+        }
+
+        x += 1
+    }
+    
 }
